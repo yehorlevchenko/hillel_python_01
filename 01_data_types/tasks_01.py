c47a8133dfcf6c4c1f65c7dc29c04703dd47451d
leprechauns_pot <yehorlevchenko@icloud.com>
2020-02-23 00:46:21 +0200

    tasks_01 are ready

---
+++ 01_data_types/tasks_01.py
@@ -0,0 +1,174 @@
+def catalog_finder(url_list):
+    """
+    Дописать функцию, которая принимает список URL, а возвращает
+    список только тех URL, в которых есть /catalog/
+    """
+    # your code here
+    result_list = None
+    return result_list
+
+
+def idiotic_str(input_str):
+    """
+    Вернуть полученную строку, сделав каждую вторую букву заглавной:
+    Пример: тестовая строка -> тЕсТоВаЯ СтРоКа
+    """
+    # your code here
+    idiotic_str = None
+    return idiotic_str
+
+
+def get_str_center(input_str):
+    """
+    Дописать функцию, которая вернет Х символов из середины строки
+    (2 для четного кол-ва символов, 3 - для нечетного).
+    """
+    # your code here
+    output_str = None
+    return output_str
+
+
+def count_symbols(input_str):
+    """
+    Дописать функцию, которая считает сколько раз каждая из букв
+    встречается в строке, разложить буквы в словарь парами
+    {буква:количество упоминаний в строке}
+    """
+    # your code here
+    output_dict = None
+    return output_dict
+
+
+def mix_strings(str1, str2):
+    """
+    Дописать функцию, которая будет принимать 2 строки и вставлять вторую
+    в середину первой
+    """
+    # your code here
+    result_str = None
+    return result_str
+
+
+def avg_score(score_list):
+    """
+    Дописать функцию, которая принимает список строк с оценками, а возвращает
+    список строк со средним баллом
+    Пример: ["Mike|83, 90, 34, 54", "Jane|45, 46, 53, 23"] ->
+    ["Mike|65", "Jane|42"]
+    """
+    # your code here
+    avg_scores = None
+    return avg_scores
+
+
+def encrypt_str(input_str):
+    """
+    Дописать функцию, которая будет шифровать полученную строку следующим
+    образом:
+    Пример 1: "www" -> "w3"
+    Пример 2: "abbbccdeffgggg" -> "ab3c2def2g4"
+    """
+    # your code here
+    encrypted_str = None
+    return encrypted_str
+
+
+def square_dict(input_dict):
+    """
+    Сгенерировать dict() из списка ключей ниже по формуле (key : key*key).
+    keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    ожидаемый результат: {1: 1, 2: 4, 3: 9 …}
+    """
+    # your code here
+    squared_dict = None
+    return squared_dict
+
+
+def even_int_generator():
+    """
+    Сгенерировать список из диапазона чисел от 0 до 100 и записать
+    в результирующий список только четные числа.
+    """
+    # your code here
+    even_int_list = None
+    return even_int_list
+
+
+def replace_vowels(input_str):
+    """
+    Заменить в произвольной строке согласные буквы на гласные.
+    """
+    # your code here
+    result_str = None
+    return result_str
+
+
+def filter_unique_int(input_list):
+    """
+    Дан массив чисел.
+    [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
+    убрать из него повторяющиеся элементы
+    """
+    # your code here
+    unique_int_list = None
+    return unique_int_list
+
+
+def three_biggest_int(input_list):
+    """
+    Дан массив чисел.
+    [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
+    вывести 3 наибольших числа из исходного массива
+    """
+    # your code here
+    biggest_ints = None
+    return biggest_ints
+
+
+def lowest_int_index(input_list):
+    """
+    Дан массив чисел.
+    [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
+    вывести индекс минимального элемента массива
+    """
+    # your code here
+    lowest_int_index = None
+    return lowest_int_index
+
+
+def reversed_list(input_list):
+    """
+    Дан массив чисел.
+    [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
+    вывести исходный массив в обратном порядке
+    """
+    # your code here
+    reversed = None
+    return reversed
+
+
+def find_common_keys(dict1, dict2):
+    """
+    Найти общие ключи в двух словарях, вернуть список их названий
+    """
+    common_keys = None
+    return common_keys
+
+
+def sort_by_age(student_list):
+    """
+    Дан массив из словарей. C помощью sort() отсортировать массив из словарей
+    по значению ключа 'age', сгруппировать данные по значению ключа 'city'
+    вывод должен быть такого вида :
+        {
+           'Kiev': [ {'name': 'Viktor', 'age': 30 },
+                        {'name': 'Andrey', 'age': 34}],
+           'Dnepr': [ {'name': 'Maksim', 'age': 20 },
+                           {'name': 'Artem', 'age': 50}],
+           'Lviv': [ {'name': 'Vladimir', 'age': 32 },
+                        {'name': 'Dmitriy', 'age': 21}]
+        }
+    """
+    # your code here
+    sorted_dict = None
+    return sorted_dict
